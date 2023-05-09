b3e11cd3615efb6437edc51d747dbd54838a8e3b
Larissa <contatolarisn@gmail.com>
2023-05-09 19:47:38 -0300

    adicionando arquivos

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const jsonServer = require("json-server")
+const server = jsonServer.create()
+const router = jsonServer.router("db.json")
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 5000
+
+server.use(middlewares)
+server.use(router)
+server.listen(port)
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
