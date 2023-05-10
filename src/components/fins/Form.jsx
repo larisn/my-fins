f1c116a97421501d1134bc239cb1763add953af2
Larissa <contatolarisn@gmail.com>
2023-05-10 17:48:22 -0300

    update

--- src/components/fins/Form.jsx
+++ src/components/fins/Form.jsx
@@ -12,7 +12,7 @@ export default function Form({ handleSubmit, btnText, finsData }) {
   const [fins, setFins] = useState(finsData || {})
 
   useEffect(() => {
-    fetch("http://localhost:5000/categories", {
+    fetch("https://my-fins.vercel.app/categories", {
       method: "GET",
       headers: {
         "Content-type": "application/json",
